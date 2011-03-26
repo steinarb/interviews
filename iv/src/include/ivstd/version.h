85851fe1104d0fac7025dd1609e8637aa625e05d
Steinar Bang <sb@edwards.hjemme.lan>
2011-03-26 01:39:51 +0100

    Importing ivtools 0.9.2 tarball, dated April 5 2001. Renamed top directory to iv to match paths with InterViews.

--- iv/src/include/ivstd/version.h
+++ iv/src/include/ivstd/version.h
@@ -1,3 +1,3 @@
-#define IvtoolsVersion 0.8.4
-#define VersionString "0.8.4"
-#define ReleaseString "ivtools-0.8.4"
+#define IvtoolsVersion 0.9.2
+#define VersionString "0.9.2"
+#define ReleaseString "ivtools-0.9.2"
